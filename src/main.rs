b202e75b3515b38bfeffc21653200f15df0c5f42
JatinSharma1908 <sharma.jatin2202@gmail.com>
2026-05-20 09:56:52 +0530

    changed port to 8000

--- src/main.rs
+++ src/main.rs
@@ -73,10 +73,10 @@ async fn main() {
         .layer(CorsLayer::permissive())
         .with_state(schema);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000")
         .await
         .unwrap();
 
-    tracing::info!("GraphQL Playground → http://localhost:3000");
+    tracing::info!("GraphQL Playground → http://localhost:8000");
     axum::serve(listener, app).await.unwrap();
 }
